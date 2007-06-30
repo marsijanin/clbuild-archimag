469ea78c5d8b45218b5f0d14f8d3268f06d788bc
david <david@lichteblau.com>
2007-07-01 00:54:46 +0400

    added hunchenhoot
    darcs-hash:20070630205446-e47ce-2b000ec1217ba389ce4914561b684c53f798d2eb.gz

--- update.sh
+++ update.sh
@@ -7,6 +7,8 @@ get_cvs_clnet gsharp
 get_cvs_clnet climacs
 get_cvs_clnet slime
 get_cvs_clnet beirc
+get_cvs_clnet cl+ssl cl-plus-ssl
+get_cvs_clnet trivial-gray-streams cl-plus-ssl
 
 get_svn_clnet usocket usocket/trunk
 get_svn_clnet cl-irc trunk
@@ -14,6 +16,10 @@ get_svn_clnet cl-irc trunk
 get_ediware cl-ppcre
 get_ediware flexi-streams
 get_ediware cl-fad
+get_ediware hunchentoot
+get_ediware chunga
+get_ediware url-rewrite
+get_ediware cl-who
 
 get_darcs clx http://common-lisp.net/~crhodes/clx
 #get_darcs cffi http://common-lisp.net/project/cffi/darcs/cffi/
@@ -30,14 +36,16 @@ get_tarball skippy http://www.xach.com/lisp/skippy.tgz
 get_tarball salza http://www.xach.com/lisp/salza/salza-0.7.2.tar.gz
 get_tarball puri http://files.b9.com/puri/puri-latest.tar.gz
 get_tarball midi http://doc.gold.ac.uk/isms/lisp/midi/midi.tar.gz
+get_tarball md5 http://files.b9.com/md5/md5-1.8.5.tar.gz
+get_tarball cl-base64 http://files.b9.com/cl-base64/cl-base64-latest.tar.gz
 get_tarball spatial-trees \
     http://ftp.linux.org.uk/pub/lisp/cclan/spatial-trees.tar.gz
-get_tarball trivial-gray-streams \
-    http://common-lisp.net/project/cl-plus-ssl/download/trivial-gray-streams.tar.gz
 get_tarball trivial-sockets \
     http://ftp.linux.org.uk/pub/lisp/cclan/trivial-sockets.tar.gz
 get_tarball split-sequence \
     http://ftp.linux.org.uk/pub/lisp/cclan/split-sequence.tar.gz
+get_tarball rfc2388 \
+    http://common-lisp.net/project/rfc2388/rfc2388_latest.tar.gz
 
 # old standalone tab-layout (for climplayer?)
 get_tarball_bz2 tab-layout http://bl0rg.net/~mgr/flux/tab-layout_2005-09-19_02-52+0200.tar.bz2
